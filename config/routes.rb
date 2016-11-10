0a2341b890888b86360fdcd4d9f39817027482af
janenyasoro <janenyasoro@gmail.com>
2016-11-10 16:55:18 +0300

    creating users using users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 	root 'static_pages#home'
     get 'signup'  => 'users#new'
+    get 'login'  => 'sessions#new'
+    post 'login' => 'sessions#create'
+    delete 'logout' => 'sessions#destroy'
     resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
