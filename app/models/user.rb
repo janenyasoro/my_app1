775e63fad0ea1dbc25e4875590cc2774841041f3
janenyasoro <janenyasoro@gmail.com>
2016-11-11 18:11:41 +0300

    creating sessions while logging in and logging out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true
+	has_secure_password
+	def User.digest(string)
+       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+          BCrypt::Engine.cost
+         BCrypt::Password.create(string, cost: cost)
+    end
+	
 end
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:jane)
+    @other_user = users(:eunice)
+  end
   test "should get new" do
-    get users_new_url
+    get :new
     assert_response :success
   end
-
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+   end
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  test "should redirect destroy when not logged in" do
+     assert_no_difference 'User.count' do
+     delete :destroy, id: @user
+     # delete delete_user_path params:{id: @user}
+     # delete user_url(@user)
+  end
+   assert_redirected_to login_url
+end
 end
